a8a02c6911b409eb944e7d4c186c925e6dacca89
SantaSpeen <santaspeen@yandex.ru>
2022-03-15 23:55:07 +0300

    0.0.1

---
+++ gitflic/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "gitflic"
+__version__ = "0.0.1"
+__description__ = "GitflicApi wrapper"
+__author__ = "SantaSpeen"
+__author_email__ = "dir@sssr.dev"
+__url__ = "https://github.com/SantaSpeen/gitflic"
+__license__ = "Apache License 2.0 "
---
+++ gitflic/api.py
@@ -0,0 +1,6 @@
+
+class Gitflic:
+
+    def __init__(self):
+        pass
+    
